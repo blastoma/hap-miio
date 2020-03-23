2b9a048c74c06bada417d52e660dc279140560d2
Nikolay Borisov <nikolay@borisov.me>
2020-03-24 00:42:54 +0300

    add dummy switch trigger

--- accessories/__init__.py
+++ accessories/__init__.py
@@ -2,3 +2,4 @@ from accessories.air_quality_monitor import AirQualityMonitor
 from accessories.vacuum import Vacuum
 from accessories.air_fresh import AirFresh
 from accessories.presence import Presence
+from accessories.dummy_switch import DummySwitch
---
+++ accessories/dummy_switch.py
@@ -0,0 +1,33 @@
+import logging
+
+from pyhap.accessory import Accessory
+from pyhap.const import CATEGORY_SWITCH, CATEGORY_PROGRAMMABLE_SWITCH
+
+logger = logging.getLogger(__name__)
+
+UPDATE_INTERVAL = 30
+
+
+class DummySwitch(Accessory):
+
+    category = CATEGORY_SWITCH
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        serv_switch = self.add_preload_service('Switch')
+        self.on = serv_switch.configure_char('On', setter_callback=self.set_on)
+
+    def set_on(self, value):
+        logger.debug('set_on(%s)', value)
+
+    def add_info_service(self):
+        info_service = self.driver.loader.get_service('AccessoryInformation')
+        info_service.configure_char('Name', value='Dummy Switch')
+        info_service.configure_char('SerialNumber', value='dummy.switch')
+        info_service.configure_char('Manufacturer', value='Nikolay Borisov')
+        self.add_service(info_service)
+
+    @Accessory.run_at_interval(UPDATE_INTERVAL)
+    async def run(self):
+        self.on.set_value(self.on.get_value() == False)
